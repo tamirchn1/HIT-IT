ddb6833d79888819c7c0ff53c1d9a7f89aedcd8d
Tamirchn1@gmail.com <T2151991t>
2021-02-02 14:34:44 +0100

    restored git

--- index.js
+++ index.js
@@ -1,29 +1,82 @@
-
-//detecting Button Press
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
 for (var i = 0; i < numberOfDrumButtons; i++) {
+
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
     var buttonInnerHTML = this.innerHTML;
+
     makeSound(buttonInnerHTML);
-    buttonAnimation(buttonInnerHTML)
+
+    buttonAnimation(buttonInnerHTML);
+
   });
+
 }
-//detecting Keyboard Press
+
 document.addEventListener("keypress", function(event) {
-    makeSound(event.key);
-    buttonAnimation(event.key)
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
 });
+
+
 function makeSound(key) {
-switch (key){
- 
-  default:
-    console.log(buttonInnerHTML);
+
+  switch (key) {
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "f":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "g":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "h":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "s":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "w":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "a":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+
+    default:aconsole.log(key);
+
   }
 }
+
+
 function buttonAnimation(currentKey) {
+
   var activeButton = document.querySelector("." + currentKey);
+
   activeButton.classList.add("pressed");
-  setTimeout(function(){
-  activeButton.classList.remove("pressed");
-}, 100);
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
 }
+a
